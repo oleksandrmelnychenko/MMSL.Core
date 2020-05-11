aecf3aa99b46a78fda3efadfa08b8e900f0f951d
Oleksandr Melnychenko <company@nolt-technologies.com>
2020-05-11 17:47:10 +0300

    improved

--- MMSL.Server.Core/Program.cs
+++ MMSL.Server.Core/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MMSL.Core
+namespace MMSL.Server.Core
 {
     public class Program
     {
--- MMSL.Server.Core/Startup.cs
+++ MMSL.Server.Core/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MMSL.Core
+namespace MMSL.Server.Core
 {
     public class Startup
     {
