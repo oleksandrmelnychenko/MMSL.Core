582c2ca61ae0012b30afbecd599ca91818930ac2
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-12 12:20:56 +0300

    Added migration for dealer entities

---
+++ MMSL.Databases/Migrations/20200512091812_AddedDealerAccountEntities.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMSL.Databases.Migrations
+{
+    public partial class AddedDealerAccountEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: true),
+                    LastModified = table.Column<DateTime>(nullable: true),
+                    AddressLine1 = table.Column<string>(nullable: true),
+                    AddressLine2 = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DealerAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: true),
+                    LastModified = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    CompanyName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    AlternateEmail = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    TaxNumber = table.Column<string>(nullable: true),
+                    IsVatApplicable = table.Column<bool>(nullable: false),
+                    Currency = table.Column<int>(nullable: false),
+                    PaymentType = table.Column<int>(nullable: false),
+                    IsCreditAllowed = table.Column<bool>(nullable: false),
+                    BillingAddressId = table.Column<long>(nullable: true),
+                    UseBillingAsShipping = table.Column<bool>(nullable: false),
+                    ShippingAddressId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DealerAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DealerAccounts_Addresses_BillingAddressId",
+                        column: x => x.BillingAddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DealerAccounts_Addresses_ShippingAddressId",
+                        column: x => x.ShippingAddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DealerAccounts_BillingAddressId",
+                table: "DealerAccounts",
+                column: "BillingAddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DealerAccounts_ShippingAddressId",
+                table: "DealerAccounts",
+                column: "ShippingAddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DealerAccounts");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
