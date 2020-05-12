ff30e1aa147638060cdfe8d3d060faa3de7081a8
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-12 13:57:03 +0300

    Added dealer repositories

---
+++ MMSL.Domain/Repositories/Addresses/Contracts/IAddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using MMSL.Domain.Entities.Addresses;
+
+namespace MMSL.Domain.Repositories.Addresses.Contracts {
+    public interface IAddressRepository {
+
+        Address GetAddress(long addressId);
+
+        long AddAddress(Address address);
+        
+        void UpdateAddress(Address address);
+
+    }
+}
---
+++ MMSL.Domain/Repositories/Dealer/Contracts/IDealerAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using MMSL.Domain.Entities.Dealer;
+
+namespace MMSL.Domain.Repositories.Dealer.Contracts {
+    public interface IDealerAccountRepository {
+
+        DealerAccount GetAddress(long dealerAccountId);
+
+        long AddAddress(DealerAccount dealerAccount);
+
+        void UpdateAddress(DealerAccount dealerAccount);
+
+    }
+}
