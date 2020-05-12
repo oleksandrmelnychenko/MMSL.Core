fa56b03a2d0739163e6ac4aa5e7108d6f2301fc8
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-12 15:22:47 +0300

    added dealer account controller

--- MMSL.Common/WebApi/RoutingConfiguration/ApplicationSegments.cs
+++ MMSL.Common/WebApi/RoutingConfiguration/ApplicationSegments.cs
@@ -1,5 +1,6 @@
 ﻿namespace MMSL.Common.WebApi.RoutingConfiguration {
     public static class ApplicationSegments {
         public const string UserIdentity = "identity";
+        public const string Dealer = "dealer/accounts";
     }
 }
---
+++ MMSL.Common/WebApi/RoutingConfiguration/DealerSegments.cs
@@ -0,0 +1,9 @@
+﻿namespace MMSL.Common.WebApi.RoutingConfiguration {
+    public static class DealerSegments {
+        public const string GET_ALL_DEALERS = "get/all";
+        public const string GET_DEALER = "get";
+        public const string ADD_DEALER = "add";
+        public const string DELETE_DEALER = "delete";
+        public const string UPDATE_DEALER = "update";
+    }
+}
