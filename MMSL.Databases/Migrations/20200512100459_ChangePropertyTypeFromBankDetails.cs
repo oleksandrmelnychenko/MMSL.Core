1499cabbfd8ed23fb88633fb99c07b75fbe1051a
Vladimir Ivanov <ivanov_va@hotmail.com>
2020-05-12 13:07:09 +0300

    Change property type BankDetail

---
+++ MMSL.Databases/Migrations/20200512100459_ChangePropertyTypeFromBankDetails.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMSL.Databases.Migrations
+{
+    public partial class ChangePropertyTypeFromBankDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VatNumber",
+                table: "BankDetails",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AccountNo",
+                table: "BankDetails",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VatNumber",
+                table: "BankDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountNo",
+                table: "BankDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+        }
+    }
+}
