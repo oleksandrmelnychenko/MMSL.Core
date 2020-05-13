0a85338bbb7f36a0e4c2117cb388536338e358b3
Vladimir Ivanov <ivanov_va@hotmail.com>
2020-05-13 17:15:17 +0300

    Added StoreCustomer repository and service

---
+++ MMSL.Services/StoreCustomerServices/Contracts/IStoreCustomerService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMSL.Services.StoreCustomerServices.Contracts {
+    public interface IStoreCustomerService {
+    }
+}
