3b4b7f4f7925a651391e39f32c690b5053991bd6
Vladimir Ivanov <ivanov_va@hotmail.com>
2020-05-14 12:46:16 +0300

    Added option group map

--- MMSL.Databases/MMSLDbContext.cs
+++ MMSL.Databases/MMSLDbContext.cs
@@ -14,38 +14,44 @@ using MMSL.Domain.Entities.Options;
 using MMSL.Databases.TableMaps.Options;
 
 namespace MMSL.Databases {
-    public class MMSLDbContext : DbContext {
-        public MMSLDbContext() { }
+    public class MMSLDbContext : DbContext {       
+
+        public DbSet<Store> Stores { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<OptionUnit> OptionUnits { get; set; }
+
+        public DbSet<OptionGroup> OptionGroups { get; set; }
 
         public DbSet<UserIdentity> UserIdentities { get; set; }
 
         public DbSet<DealerAccount> DealerAccounts { get; set; }
 
-        public DbSet<Address> Addresses { get; set; }
-
-        public DbSet<Store> Stores { get; set; }
+        public DbSet<StoreCustomer> StoreCustomers { get; set; }
 
         public DbSet<StoreMapDealerAccount> StoreDealerAccounts { get; set; }
 
-        public DbSet<StoreCustomer> StoreCustomers { get; set; }
-
-        public DbSet<OptionUnit> OptionUnits { get; set; }
-
-        public MMSLDbContext(
-            DbContextOptions<MMSLDbContext> options) : base(options) { }
+        /// <summary>
+        ///     ctor().
+        /// </summary>
+        public MMSLDbContext() { }
+       
+        public MMSLDbContext(DbContextOptions<MMSLDbContext> options) : base(options) { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlServer("Data Source=78.152.175.67;Initial Catalog=Mmsl;Integrated Security=False;User ID=ef_migrator;Password=Grimm_jow92;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.AddConfiguration(new StoreMap());
+            modelBuilder.AddConfiguration(new AddressMap());
+            modelBuilder.AddConfiguration(new OptionUnitMap());
+            modelBuilder.AddConfiguration(new OptionGroupMap());
             modelBuilder.AddConfiguration(new UserIdentityMap());
             modelBuilder.AddConfiguration(new DealerAccountMap());
-            modelBuilder.AddConfiguration(new AddressMap());
-            modelBuilder.AddConfiguration(new StoreMap());
-            modelBuilder.AddConfiguration(new StoreDealerAccountMap());
             modelBuilder.AddConfiguration(new StoreCustomersMap());
-            modelBuilder.AddConfiguration(new OptionUnitMap());
+            modelBuilder.AddConfiguration(new StoreDealerAccountMap());
         }
     }
 }
---
+++ MMSL.Databases/TableMaps/Options/OptionGroupMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MMSL.Domain.Entities.Options;
+
+namespace MMSL.Databases.TableMaps.Options {
+    public class OptionGroupMap : EntityBaseMap<OptionGroup> {
+        public override void Map(EntityTypeBuilder<OptionGroup> entity) {
+            base.Map(entity);
+            entity.ToTable("OptionGroups");
+        }
+    }
+}
