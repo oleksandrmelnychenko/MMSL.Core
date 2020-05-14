19be28cce34a09ed9626088d9a3b8c595114d35c
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-14 12:39:26 +0300

    extends option unit entity and added map

--- MMSL.Domain/Entities/Options/OptionUnit.cs
+++ MMSL.Domain/Entities/Options/OptionUnit.cs
@@ -1,4 +1,13 @@
-﻿namespace MMSL.Domain.Entities.Options {
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MMSL.Domain.Entities.Options {
     public class OptionUnit : EntityBase {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string ImageUrl { get; set; }
+
+        public long OptionGroupId { get; set; }
+
+        public OptionGroup OptionGroup { get; set; }
     }
 }
