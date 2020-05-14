1bb772df1b1a2546fa56778217c9e475d9a88414
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-14 12:51:29 +0300

    Added migration for options group and units

---
+++ MMSL.Databases/Migrations/20200514094700_AddedOptionsGroupAndUnitsEntities.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMSL.Databases.Migrations
+{
+    public partial class AddedOptionsGroupAndUnitsEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OptionGroups",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: true, defaultValueSql: "getutcdate()"),
+                    LastModified = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OptionGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OptionUnits",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: true, defaultValueSql: "getutcdate()"),
+                    LastModified = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    OptionGroupId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OptionUnits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OptionUnits_OptionGroups_OptionGroupId",
+                        column: x => x.OptionGroupId,
+                        principalTable: "OptionGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OptionUnits_OptionGroupId",
+                table: "OptionUnits",
+                column: "OptionGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OptionUnits");
+
+            migrationBuilder.DropTable(
+                name: "OptionGroups");
+        }
+    }
+}
