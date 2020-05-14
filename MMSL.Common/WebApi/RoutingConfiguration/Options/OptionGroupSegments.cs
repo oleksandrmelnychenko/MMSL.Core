21380af0b167de0e85df6ceef439e757752300cc
Vladimir Ivanov <ivanov_va@hotmail.com>
2020-05-14 14:23:39 +0300

    Minor fix

--- MMSL.Common/WebApi/RoutingConfiguration/Options/OptionGroupSegments.cs
+++ MMSL.Common/WebApi/RoutingConfiguration/Options/OptionGroupSegments.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace MMSL.Common.WebApi.RoutingConfiguration.Options {
     public static class OptionGroupSegments {
 
-        public const string GET_OPTION_GROUPS = "get/all/option/group";
+        public const string GET_OPTION_GROUPS = "groups/get/all";
 
     }
 }
