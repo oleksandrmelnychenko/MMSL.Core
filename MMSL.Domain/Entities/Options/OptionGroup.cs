30886849c107787d18f24cf65120e5ba18488db9
Vladimir Ivanov <ivanov_va@hotmail.com>
2020-05-14 12:40:49 +0300

    Minor update

--- MMSL.Domain/Entities/Options/OptionGroup.cs
+++ MMSL.Domain/Entities/Options/OptionGroup.cs
@@ -1,4 +1,11 @@
-﻿namespace MMSL.Domain.Entities.Options {
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MMSL.Domain.Entities.Options {
     public class OptionGroup : EntityBase {
+
+        public string Name { get; set; }
+
+        public ICollection<OptionUnit> OptionUnits { get; set; }
     }
 }
