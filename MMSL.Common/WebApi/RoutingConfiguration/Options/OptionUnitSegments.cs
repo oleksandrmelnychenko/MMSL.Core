87cd4e93fb253028efcfbf941b6870e71a814759
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-14 14:31:20 +0300

    added option unit crud endpoints and updated option unit service

--- MMSL.Common/WebApi/RoutingConfiguration/Options/OptionUnitSegments.cs
+++ MMSL.Common/WebApi/RoutingConfiguration/Options/OptionUnitSegments.cs
@@ -1,5 +1,9 @@
 ﻿namespace MMSL.Common.WebApi.RoutingConfiguration.Options {
     public static class OptionUnitSegments {
         public const string GET_OPTION_UNITS = "units/get";
+        public const string GET_GROUP_OPTION_UNITS = "group/units/get";
+        public const string ADD_OPTION_UNIT = "units/add";
+        public const string UPDATE_OPTION_UNIT = "units/update";
+        public const string DELETE_OPTION_UNIT = "units/delete";
     }
 }
--- MMSL.Server.Core/Controllers/Options/OptionUnitsControllers.cs
+++ MMSL.Server.Core/Controllers/Options/OptionUnitsControllers.cs
@@ -5,6 +5,7 @@ using MMSL.Common.ResponseBuilder.Contracts;
 using MMSL.Common.WebApi;
 using MMSL.Common.WebApi.RoutingConfiguration;
 using MMSL.Common.WebApi.RoutingConfiguration.Options;
+using MMSL.Domain.Entities.Options;
 using MMSL.Services.OptionServices.Contracts;
 using Serilog;
 using System;
@@ -29,6 +30,50 @@ namespace MMSL.Server.Core.Controllers.Options {
         public async Task<IActionResult> GetOptionUnit([FromQuery]long optionUnitId) {
             try {
                 return Ok(SuccessResponseBody(await _optionUnitService.GetOptionUnitByIdAsync(optionUnitId), Localizer["Successfully completed"]));
+            } catch (Exception exc) {
+                Log.Error(exc.Message);
+                return BadRequest(ErrorResponseBody(exc.Message, HttpStatusCode.BadRequest));
+            }
+        }
+
+        [HttpGet]
+        [AssignActionRoute(OptionUnitSegments.GET_GROUP_OPTION_UNITS)]
+        public async Task<IActionResult> GetGroupOptionUnit([FromQuery]long optionGroupId) {
+            try {
+                return Ok(SuccessResponseBody(await _optionUnitService.GetOptionUnitsByGroupIdAsync(optionGroupId), Localizer["Successfully completed"]));
+            } catch (Exception exc) {
+                Log.Error(exc.Message);
+                return BadRequest(ErrorResponseBody(exc.Message, HttpStatusCode.BadRequest));
+            }
+        }
+
+        [HttpPost]
+        [AssignActionRoute(OptionUnitSegments.ADD_OPTION_UNIT)]
+        public async Task<IActionResult> AddOptionUnit([FromBody]OptionUnit optionUnit) {
+            try {
+                return Ok(SuccessResponseBody(await _optionUnitService.AddOptionUnit(optionUnit), Localizer["Successfully created"]));
+            } catch (Exception exc) {
+                Log.Error(exc.Message);
+                return BadRequest(ErrorResponseBody(exc.Message, HttpStatusCode.BadRequest));
+            }
+        }
+
+        [HttpPut]
+        [AssignActionRoute(OptionUnitSegments.UPDATE_OPTION_UNIT)]
+        public async Task<IActionResult> UpdateOptionUnit([FromBody]OptionUnit optionUnit) {
+            try {
+                return Ok(SuccessResponseBody(await _optionUnitService.UpdateOptionUnit(optionUnit), Localizer["Successfully updated"]));
+            } catch (Exception exc) {
+                Log.Error(exc.Message);
+                return BadRequest(ErrorResponseBody(exc.Message, HttpStatusCode.BadRequest));
+            }
+        }
+
+        [HttpDelete]
+        [AssignActionRoute(OptionUnitSegments.DELETE_OPTION_UNIT)]
+        public async Task<IActionResult> DeleteOptionUnit([FromQuery]long optionUnitId) {
+            try {
+                return Ok(SuccessResponseBody(await _optionUnitService.DeleteOptionUnit(optionUnitId), Localizer["Successfully updated"]));
             } catch (Exception exc) {
                 Log.Error(exc.Message);
                 return BadRequest(ErrorResponseBody(exc.Message, HttpStatusCode.BadRequest));
