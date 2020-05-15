5a0b198948a362997c09b693bf4d37f01c7e4d9c
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-15 13:13:45 +0300

    added swagger to work with api

--- MMSL.Server.Core/Startup.cs
+++ MMSL.Server.Core/Startup.cs
@@ -65,6 +65,7 @@ using MMSL.Services.Types;
 using MMSL.Services.Types.Contracts;
 using MMSL.Domain.Repositories.Types;
 using MMSL.Domain.Repositories.Types.Contracts;
+using Microsoft.OpenApi.Models;
 
 namespace MMSL.Server.Core
 {
@@ -144,6 +145,12 @@ namespace MMSL.Server.Core
                 t => new SqlDbContext(new MMSLDbContext(t.GetService<DbContextOptions<MMSLDbContext>>())), ServiceLifetime.Transient)
             );
 
+#if DEBUG
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MMSL API", Version = "v1" });
+            });
+#endif
+
             ContainerBuilder builder = new ContainerBuilder();
 
             builder.Populate(services);
@@ -204,6 +211,15 @@ namespace MMSL.Server.Core
                 (new JsonFormatter(), "logs\\devInfo.json");
 
             Log.Logger = logger.CreateLogger();
+
+#if DEBUG
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MMSL API V1");
+                c.RoutePrefix = string.Empty;
+            });
+#endif
 
             app.UseCors("CorsPolicy");
 
