cc2296698ba7828a2e416240364f0d050b2602cb
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-15 13:49:39 +0300

    added auth settings for swagger

--- MMSL.Server.Core/Startup.cs
+++ MMSL.Server.Core/Startup.cs
@@ -148,6 +148,24 @@ namespace MMSL.Server.Core
 #if DEBUG
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MMSL API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }, new string[] { }
+                    }
+                });
             });
 #endif
 
