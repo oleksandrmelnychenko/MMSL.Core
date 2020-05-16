d717349894da51ddd25ae781ba7dd058429a6bb8
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-16 13:24:16 +0300

    Added data contract base models

---
+++ MMSL.Domain/DataContracts/EntityDataContractBase.cs
@@ -0,0 +1,8 @@
+﻿using MMSL.Domain.Entities;
+
+namespace MMSL.Domain.DataContracts {
+    public abstract class EntityDataContractBase<TEntity> where TEntity : EntityBaseNamed {
+        public long Id { get; set; }
+        public abstract TEntity GetEntity();
+    }
+}
---
+++ MMSL.Domain/DataContracts/NamedEntityDataContractBase.cs
@@ -0,0 +1,8 @@
+﻿using MMSL.Domain.Entities;
+
+namespace MMSL.Domain.DataContracts {
+    public abstract class NamedEntityDataContractBase<TEntity> : EntityDataContractBase<TEntity> where TEntity : EntityBaseNamed {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
