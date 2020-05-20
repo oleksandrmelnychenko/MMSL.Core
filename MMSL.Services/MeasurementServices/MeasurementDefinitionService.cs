af4a1a2e16ea2cfd52a622c5304fdb6b3f7ea455
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-05-20 17:27:52 +0300

    added measurement repository factory to services

--- MMSL.Services/MeasurementServices/MeasurementDefinitionService.cs
+++ MMSL.Services/MeasurementServices/MeasurementDefinitionService.cs
@@ -1,6 +1,12 @@
-﻿using MMSL.Services.MeasurementServices.Contracts;
+﻿using MMSL.Domain.Repositories.Measurements.Contracts;
+using MMSL.Services.MeasurementServices.Contracts;
 
 namespace MMSL.Services.MeasurementServices {
     public class MeasurementDefinitionService : IMeasurementDefinitionService {
+        private readonly IMeasurementsRepositoriesFactory _measurementsRepositoriesFactory;
+
+        public MeasurementDefinitionService(IMeasurementsRepositoriesFactory measurementsRepositoriesFactory) {
+            _measurementsRepositoriesFactory = measurementsRepositoriesFactory;
+        }
     }
 }
--- MMSL.Services/MeasurementServices/MeasurementSizeService.cs
+++ MMSL.Services/MeasurementServices/MeasurementSizeService.cs
@@ -1,6 +1,12 @@
-﻿using MMSL.Services.MeasurementServices.Contracts;
+﻿using MMSL.Domain.Repositories.Measurements.Contracts;
+using MMSL.Services.MeasurementServices.Contracts;
 
 namespace MMSL.Services.MeasurementServices {
     public class MeasurementSizeService : IMeasurementSizeService {
+        private readonly IMeasurementsRepositoriesFactory _measurementsRepositoriesFactory;
+
+        public MeasurementSizeService(IMeasurementsRepositoriesFactory measurementsRepositoriesFactory) {
+            _measurementsRepositoriesFactory = measurementsRepositoriesFactory;
+        }
     }
 }
