dddf32953fc7fdcf85644c79932dac4d194e2b29
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-06-25 11:20:14 +0300

    updated unit value data contrtact to string value

--- MMSL.Domain/DataContracts/ProductOptions/UnitValueDataContract.cs
+++ MMSL.Domain/DataContracts/ProductOptions/UnitValueDataContract.cs
@@ -1,7 +1,7 @@
 ﻿namespace MMSL.Domain.DataContracts.ProductOptions {
     public class UnitValueDataContract {
         public long Id { get; set; }
-        public float Value { get; set; }
+        public string Value { get; set; }
         public bool IsDeleted { get; set; }
         public int OrderIndex { get; set; }
     }
