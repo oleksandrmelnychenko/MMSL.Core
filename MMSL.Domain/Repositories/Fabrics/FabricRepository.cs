6af6012c3b65807fa1d6af1b35e3652a17e73156
Serhiy Matviyuk <serhij.matvijuk@gmail.com>
2020-07-08 17:19:42 +0300

    fixed adding and searching

--- MMSL.Domain/Repositories/Fabrics/FabricRepository.cs
+++ MMSL.Domain/Repositories/Fabrics/FabricRepository.cs
@@ -18,40 +18,60 @@ namespace MMSL.Domain.Repositories.Fabrics {
 
             PagerParams pager = new PagerParams(pageNumber, limit, searchPhrase);
 
-            string paginatingDetailQuery = 
-@"SELECT TOP(1) 
+            string searchPart =
+@"AND (
+PATINDEX('%' + @SearchTerm + '%', [Fabrics].[FabricCode]) > 0 
+OR PATINDEX('%' + @SearchTerm + '%', [Fabrics].[Description]) > 0 
+)";
+
+            string paginatingDetailQuery =
+$@"SELECT TOP(1) 
 [PageSize]= @Limit,
 [PageNumber] = (@Offset / @Limit) + 1, 
 [TotalItems] = COUNT(DISTINCT [Fabrics].Id), 
 [PagesCount] = CEILING(CONVERT(float, COUNT(DISTINCT[Fabrics].Id)) / @Limit) 
 FROM [Fabrics] 
-WHERE [Fabrics].[IsDeleted] = 0";
+WHERE [Fabrics].[IsDeleted] = 0 
+{(!string.IsNullOrEmpty(searchPhrase) ? searchPart : string.Empty)}";
 
             string query =
-@";WITH [Paginated_Fabrics_CTE] AS ( 
+$@";WITH [Paginated_Fabrics_CTE] AS ( 
 SELECT [Fabrics].Id, ROW_NUMBER() OVER(ORDER BY [Fabrics].[FabricCode]) AS [RowNumber] 
 FROM [Fabrics] 
 WHERE [Fabrics].IsDeleted = 0 
+{(!string.IsNullOrEmpty(searchPhrase) ? searchPart : string.Empty)}
 )
-SELECT [Paginated_Fabrics_CTE].RowNumber, [Fabrics].*
+SELECT [Paginated_Fabrics_CTE].RowNumber
+,[Fabrics].[Id]
+,[IsDeleted]
+,[Created]
+,[LastModified]
+,[FabricCode]
+,[Description]
+,[ImageUrl]
+,[Status]
+,IIF([IsMetresVisible]=1,[Metres],NULL) AS [Metres]
+,[IsMetresVisible]
+,IIF([IsMillVisible]=1,[Mill],NULL) AS [Mill]
+,[IsMillVisible]
+,IIF([IsColorVisible]=1,[Color],NULL) AS [Color]
+,[IsColorVisible]
+,IIF([IsCompositionVisible]=1,[Composition],NULL) AS [Composition]
+,[IsCompositionVisible]
+,IIF([IsGSMVisible]=1,[GSM],NULL) AS [GSM]
+,[IsGSMVisible]
+,IIF([IsCountVisible]=1,[Count],NULL) AS [Count]
+,[IsCountVisible]
+,IIF([IsWeaveVisible]=1,[Weave],NULL) AS [Weave]
+,[IsWeaveVisible]
+,IIF([IsPatternVisible]=1,[Pattern],NULL) AS [Pattern]
+,[IsPatternVisible]
 FROM [Fabrics]  
 LEFT JOIN [Paginated_Fabrics_CTE] ON [Paginated_Fabrics_CTE].Id = [Fabrics].Id  
 WHERE [Fabrics].IsDeleted = 0  
 AND [Paginated_Fabrics_CTE].RowNumber > @Offset  
 AND [Paginated_Fabrics_CTE].RowNumber <= @Offset + @Limit  
 ORDER BY [Paginated_Fabrics_CTE].RowNumber";
-
-            if (!string.IsNullOrEmpty(searchPhrase)) {
-                string searchPart = 
-@"AND (
-PATINDEX('%' + @SearchTerm + '%', [DealerAccount].CompanyName) > 0 
-OR PATINDEX('%' + @SearchTerm + '%', [DealerAccount].Email) > 0 
-OR PATINDEX('%' + @SearchTerm + '%', [DealerAccount].PhoneNumber) > 0 
-)";
-
-                paginatingDetailQuery += searchPart;
-                query += searchPart;
-            }
 
             result.Entities = _connection.Query<Fabric>(query,
                 new {
@@ -92,7 +112,7 @@ WHERE Id = @Id",
             long id = _connection.QuerySingleOrDefault<long>(
 @"INSERT INTO [Fabrics]([IsDeleted],[IsMetresVisible],[IsMillVisible],[IsColorVisible],[IsCompositionVisible],[IsGSMVisible],[IsCountVisible],[IsWeaveVisible],[IsPatternVisible]
 ,[FabricCode],[Description],[ImageUrl],[Status],[Metres],[Mill],[Color],[Composition],[GSM],[Count],[Weave],[Pattern])
-VALUES (0,1,1,1,1,1,1,1
+VALUES (0,1,1,1,1,1,1,1,1
 ,@FabricCode,@Description,@ImageUrl,@Status,@Metres,@Mill,@Color,@Composition,@GSM,@Count,@Weave,@Pattern);
 SELECT SCOPE_IDENTITY()",
                 fabricEntity
